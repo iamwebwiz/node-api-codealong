b04c0d837f094ba44cba144ab5a5365331a0f580
Ezekiel <iamwebwiz@gmail.com>
2019-08-30 07:52:35 +0100

    Add bookRouter.js file

--- app.js
+++ app.js
@@ -5,38 +5,13 @@ const app = express();
 const db = mongoose.connect('mongodb://localhost/booksApi', {
   useNewUrlParser: true,
 });
-const bookRouter = express.Router();
-const port = process.env.PORT || 3000;
 const Book = require('./models/Book');
+const bookRouter = require('./routes/bookRouter')(Book);
+
+const port = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
-bookRouter.route('/books').get((req, res) => {
-  const query = {};
-
-  if (req.query.author) {
-    query.author = req.query.author;
-  }
-
-  Book.find(query, (err, books) => {
-    if (err) return res.send(err);
-    return res.json(books);
-  });
-});
-
-bookRouter.route('/books/:bookId').get((req, res) => {
-  Book.findById(req.params.bookId, (err, book) => {
-    if (err) return res.send(err);
-    return res.json(book);
-  });
-});
-
-bookRouter.route('/books').post((req, res) => {
-  const book = new Book(req.body);
-  console.log(book);
-  return res.json(book);
-});
 
 app.use('/api', bookRouter);
 
