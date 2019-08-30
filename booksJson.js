15581eab192753167f7fa2b61b2a606456c170bb
Ezekiel <iamwebwiz@gmail.com>
2019-08-30 06:05:35 +0100

    Added booksJson to seed database

---
+++ booksJson.js
@@ -0,0 +1,14 @@
+db.books.insert([
+  {
+    title: 'War and Peace',
+    genre: 'Historical Fiction',
+    author: 'Zuck',
+    read: false,
+  },
+  {
+    title: 'Things Fall Apart',
+    genre: 'Well',
+    author: 'Chinua Achebe',
+    read: false,
+  },
+]);
