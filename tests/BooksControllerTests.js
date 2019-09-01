fea7b188fa516af5c5eeaf6baccd693ec4870c64
Ezekiel <iamwebwiz@gmail.com>
2019-09-01 06:14:06 +0100

    Add tests

---
+++ tests/BooksControllerTests.js
@@ -0,0 +1,11 @@
+const should = require('should');
+const sinon = require('sinon');
+const BooksController = require('../controllers/BooksController');
+
+describe('Books controller tests:', () => {
+  describe('Post', () => {
+    it('should not allow an empty title on post', () => {
+      //
+    });
+  });
+});
